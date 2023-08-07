b0a8fd1bddb86956ab9b0c80df69c781bfe16c4c
Erqoziyev <erqoziyevshodiyor@gmail.com>
2023-08-08 00:12:39 +0500

    Created Login Page

--- src/onmed-desktop/MainWindow.xaml.cs
+++ src/onmed-desktop/MainWindow.xaml.cs
@@ -1,51 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace onmed_desktop
+namespace onmed_desktop;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void btnMinimize_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
+    private void btnMinimize_Click(object sender, RoutedEventArgs e)
+    {
+        this.WindowState = WindowState.Minimized;
+    }
 
-        private void btnMaximize_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.WindowState == WindowState.Maximized)
-                this.WindowState = WindowState.Normal;
-            else this.WindowState = WindowState.Maximized;
-        }
+    private void btnMaximize_Click(object sender, RoutedEventArgs e)
+    {
+        if (this.WindowState == WindowState.Maximized)
+            this.WindowState = WindowState.Normal;
+        else this.WindowState = WindowState.Maximized;
+    }
 
-        private void btnClose_Click(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
+    private void btnClose_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
 
-        private void borderDragable_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            this.DragMove();
-        }
+    private void borderDragable_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        this.DragMove();
+    }
 
+    private void btnLogin(object sender, RoutedEventArgs e)
+    {
+        mainControl.SelectedIndex = 1;
     }
 }
