269b5ecbdbf5939bff34cd1c4306ae8c24aca79f
Erqoziyev <erqoziyevshodiyor@gmail.com>
2023-08-11 00:29:21 +0500

    MainWindow is begin

---
+++ src/OnMed.Desktop/LoginWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OnMed.Desktop
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+            else this.WindowState = WindowState.Maximized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void borderDragable_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btnLogin(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tbPhoneNumber_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string text = textBox.Text;
+            string filteredText = Regex.Replace(text, "[^0-9]+", "");
+
+            if (text != filteredText)
+            {
+                int caretIndex = textBox.CaretIndex;
+                textBox.Text = filteredText;
+                textBox.CaretIndex = caretIndex > 0 ? caretIndex - 1 : 0;
+            }
+        }
+    }
+}
