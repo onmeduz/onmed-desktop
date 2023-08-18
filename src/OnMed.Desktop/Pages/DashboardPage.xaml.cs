1ba746f93f6e360154102f1edd16684e853f049b
Erqoziyev <erqoziyevshodiyor@gmail.com>
2023-08-18 11:51:26 +0500

    Created Doctor component

--- src/OnMed.Desktop/Pages/DashboardPage.xaml.cs
+++ src/OnMed.Desktop/Pages/DashboardPage.xaml.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OnMed.Desktop.Component;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace OnMed.Desktop.Pages
+namespace OnMed.Desktop.Pages;
+
+/// <summary>
+/// Interaction logic for DashboardPage.xaml
+/// </summary>
+public partial class DashboardPage : Page
 {
-    /// <summary>
-    /// Interaction logic for DashboardPage.xaml
-    /// </summary>
-    public partial class DashboardPage : Page
+    public DashboardPage()
     {
-        public DashboardPage()
+        InitializeComponent();
+    }
+
+    private void Page_Loaded(object sender, RoutedEventArgs e)
+    {
+        for (int i = 0; i < 15; i++)
         {
-            InitializeComponent();
+            DoctorControlForDashboard doctorControlForDashboard = new DoctorControlForDashboard();
+            wrpDoctors.Children.Add(doctorControlForDashboard);
+
+            UserControlForDashboard userControlForDashboard = new UserControlForDashboard();
+            wrpUsers.Children.Add(userControlForDashboard);
         }
     }
 }
