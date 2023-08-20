d39b835a349f1d05947d7b67c41fedbe9dcf4cb8
Erqoziyev <erqoziyevshodiyor@gmail.com>
2023-08-20 15:44:30 +0500

    Created CategoryComponent

---
+++ src/OnMed.Desktop/Component/CategoryComponent.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OnMed.Desktop.Component
+{
+    /// <summary>
+    /// Interaction logic for CategoryComponent.xaml
+    /// </summary>
+    public partial class CategoryComponent : UserControl
+    {
+        public CategoryComponent()
+        {
+            InitializeComponent();
+        }
+    }
+}
